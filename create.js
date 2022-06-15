fb00714f93e08b8efd2fbc13d16b316080bffc22
dcrisanto <dorelly.crisanto@gmail.com>
2022-06-14 22:56:26 -0500

    mejora en maquetación de create-account y creación de pantalla de edit-account

--- create.js
+++
@@ -1,7 +0,0 @@
-const inputSubmitCreate = document.getElementById('submit-create');
-
-inputSubmitCreate.addEventListener('click', edit = (e) => {
-    inputSubmitCreate.style.backgroundColor = '#FFFFFF';
-    inputSubmitCreate.style.color = '#ACD9B2'
-    e.srcElement.value = 'Editar';
-});
