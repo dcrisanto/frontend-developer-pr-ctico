5be21a61ce10ed66662ef973c68d25d049ac93da
dcrisanto <dorelly.crisanto@gmail.com>
2022-07-21 21:02:10 -0500

    Fusión de shoppingCart con JS

--- fusion-navbar-menu.js
+++ fusion-navbar-menu.js
@@ -6,6 +6,8 @@ const closeIconMenu = document.querySelector('.icon-close');
 const containerClose = document.querySelector('.container-close');
 const containerCategories = document.querySelector('.container-categories');
 const containerAccount = document.querySelector('.container-account');
+const iconShopping = document.querySelector('.icon-shopping-notification');
+const cartsShopping = document.querySelector('.carts-secction');
 
 const showEmail = () => {
     if(userStatus) {
@@ -20,15 +22,49 @@ const showEmail = () => {
 showEmail();
 
 const toogleMenuMobile = () => {
-    containerClose.classList.toggle('display');
-    containerCategories.classList.toggle('display');
-    containerAccount.classList.toggle('display');
+    if(cartsShopping.classList.contains('display')){
+        containerClose.classList.toggle('display');
+        containerCategories.classList.toggle('display');
+        containerAccount.classList.toggle('display');
+    } else {
+        cartsShopping.classList.add('display');
+        containerClose.classList.toggle('display');
+        containerCategories.classList.toggle('display');
+        containerAccount.classList.toggle('display');
+    }
 }
 
 const toogleMenuDesktop = () => {
-    containerAccount.classList.toggle('show');
+    const isCartsShoppingClose = cartsShopping.classList.contains('display');
+
+    if(isCartsShoppingClose){
+        containerAccount.classList.toggle('show');
+    } else {
+        cartsShopping.classList.add('display');
+        containerAccount.classList.toggle('show');
+    }
+}
+
+const toogleShopping = () => {
+    const isContainerAccountCloseMobile = containerAccount.classList.contains('display');
+    const isContainerAccountOpenDesktop = containerAccount.classList.contains('show');
+
+    if(isContainerAccountCloseMobile || isContainerAccountOpenDesktop) {
+        containerAccount.classList.remove('show');
+        containerClose.classList.add('display');
+        containerCategories.classList.add('display');
+        containerAccount.classList.add('display');
+        cartsShopping.classList.toggle('display');
+    } else {
+        containerClose.classList.add('display');
+        containerCategories.classList.add('display');
+        containerAccount.classList.add('display');
+        cartsShopping.classList.toggle('display');
+    }
+    
 }
 
 email.addEventListener('click', toogleMenuDesktop);
 menuMobile.addEventListener('click', toogleMenuMobile);
 closeIconMenu.addEventListener('click', toogleMenuMobile);
+iconShopping.addEventListener('click', toogleShopping);
